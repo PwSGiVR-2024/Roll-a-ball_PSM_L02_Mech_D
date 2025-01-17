f39ab1abad46fbbfa94b6d9d4eb83ce73f853bc9
InvisibleF0x <dominik02.mech@gmail.com>
2025-01-17 20:41:30 +0100

    added enemy explosions

--- Assets/Scripts/TurretScripts/EnemyHpHandlerScript.cs
+++ Assets/Scripts/TurretScripts/EnemyHpHandlerScript.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class EnemyHpHandlerScript : HpHandlerScript
 {
+    public GameObject Explosion;
 
     protected override void Update()
     {
         if (Hp <= 0)
         {
+            Instantiate(Explosion, transform.position, Quaternion.Euler(0, 0, 0));
             Destroy(gameObject);
         }
     }
